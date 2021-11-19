2c6dd1cac637cdc037ee91c8fb5d77e013d8117e
adebay8 <adebayop.o@gmail.com>
2021-11-19 15:53:09 +0100

    set config of ssl to no validate

--- ormconfig.js
+++ ormconfig.js
@@ -33,7 +33,7 @@ module.exports = {
   synchronize: true,
   logging: false,
   extra: {
-    ssl: envString(true, false),
+    ssl: { rejectUnauthorized: false },
   },
   entities: [
     envString("build/database/entity/**/*.js", "src/database/entity/**/*.ts"),
